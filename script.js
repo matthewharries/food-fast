13dea9ba52db16ffcd20608d8f3ddfbc1eb6ac9f
Matthew Harries <matthewharries@gmail.com>
2018-02-14 20:10:17 -0700

    Added random recipe generator

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-$(document).ready(function() {       
-    $("#search-button").click((e) =>{
+$(document).ready(function() {       	
+			$("#search-button").click((e) =>{
         e.preventDefault();
         var inputValue = $("#search-value").val();
-        var stackURL = "http://www.themealdb.com/api/json/v1/1/search.php?s=" + inputValue;
+        var currentURL = "http://www.themealdb.com/api/json/v1/1/search.php?s=" + inputValue;
         $.ajax({
-            url: stackURL,
+            url: currentURL,
             responseType: 'json',
-            success: (json) => {
-							if(json.meals != undefined){
-								var results = '<h1 id="sub-title" class="title">Results for <em>' + inputValue + '</em></h1>';
+            success: loadRecipes
+        });
+    });
+		
+		$(".is-info").click((e) =>{
+        e.preventDefault();
+        var inputValue = $("#search-value").val();
+				var randomURL = 'http://www.themealdb.com/api/json/v1/1/random.php'
+        $.ajax({
+            url: randomURL,
+            responseType: 'json',
+            success: loadRecipes
+        });
+    })
+	
+		var loadRecipes = function(json){
+			if(json.meals != undefined){
+								$("#number-results").html(json.meals.length);
+								var results = '<h1 id="sub-title" class="title">Random Recipe!</h1>';
 
 								json.meals.forEach((obj)=>{
 									results += '<div class="box"><article class="media"><div class="media-content"><div class="content"><p><strong>' + obj.strMeal + '</strong><br></p>';
@@ -30,8 +46,6 @@ $(document).ready(function() {
 								var results = '<h1 id="sub-title" class="title">No results for <em>' + inputValue + '</em></h1>';
 								$("#number-results").html(0);
 							}
-							$("#main-content").html(results);
-           }
-        });
-    });
+							$("#main-content").html(results);	
+		};
 });
