a8e8d99ebd435b55fa257e23eb4d0af8ff949b8e
Matthew Harries <matthewharries@gmail.com>
2018-02-14 19:39:08 -0700

    Formatted response

--- script.js
+++ script.js
@@ -7,16 +7,31 @@ $(document).ready(function() {
             url: stackURL,
             responseType: 'json',
             success: (json) => {
-							console.log(json)
-//                var results = "<h2>Stack Overflow Answers for: <em>'" + inputValue + "'</em> </h2><ul>";
-//                
-//                //Only display top 8 most relevant answers
-//                for (var i = 0; (i < 8 && i < json.items.length); i++){
-//                    results += "<li><a target='_blank' href='" + json.items[i].link + "'>" + json.items[i].title + "</a></li>";
-//                }
-//                results += "</ul>"
-//                $("#stackOverflowResults").html(results);
-            }
-        })
+							if(json.meals != undefined){
+								var results = '<h1 id="sub-title" class="title">Results for <em>' + inputValue + '</em></h1>';
+
+								json.meals.forEach((obj)=>{
+									results += '<div class="box"><article class="media"><div class="media-content"><div class="content"><p><strong>' + obj.strMeal + '</strong><br></p>';
+
+									results += '<em>Instructions</em><br><p>' + obj.strInstructions + '</p>';
+
+									results += '<em>Ingredients</em><ul>';
+
+									var i = 1;
+									while(obj['strIngredient' + i] != "" && i < 21){
+										results += '<li>' + obj['strIngredient' + i] +' - ' + obj['strMeasure' + i] + '</li>';
+										i++;
+									}
+
+									results += '</ul></div></div></article></div>';
+
+								});
+							}else {
+								var results = '<h1 id="sub-title" class="title">No results for <em>' + inputValue + '</em></h1>';
+								$("#number-results").html(0);
+							}
+							$("#main-content").html(results);
+           }
+        });
     });
 });
